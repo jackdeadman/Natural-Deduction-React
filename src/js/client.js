ac5edd6728e0a50f7bb5dd264ca890291c6f2026
Jack Deadman <jdeadman1@sheffield.ac.uk>
2016-06-22 18:18:41 +0100

    Front end proof of concept

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>Hello this works?</h1>
-    );
-  }
-}
+import Layout from './pages/Layout'
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
