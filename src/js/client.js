36e513a25086d0271d0219c11f21be03c35bd546
Jack Deadman <jdeadman1@sheffield.ac.uk>
2016-06-30 16:09:41 +0100

    Rename main layout to Proof

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Layout from './pages/Layout'
+import Proof from './pages/Proof'
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Proof/>, app);
