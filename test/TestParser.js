4b95ace560921dfe7ff1832de22bdc8b9565a506
Jack Deadman <jdeadman1@sheffield.ac.uk>
2016-08-28 12:48:35 +0100

    More unit tests for logic parser

--- test/TestParser.js
+++ test/TestParser.js
@@ -11,12 +11,28 @@ describe('LogicExpressionParser', function() {
       this._parser = new LogicExpressionParser();
     });
 
-    it('should detect a binary expression with only one operand is not well formed', function() {
+    it('should detect a binary operator with only no operands is not well formed', function() {
+      assert.strictEqual(this._parser.isWellformed('∧'), false);
+    });
+
+    it('should detect a binary operator with only one operand is not well formed', function() {
       assert.strictEqual(this._parser.isWellformed('a∧'), false);
     });
 
-    it('should detect a binary expression with two operands is well formed', function() {
+    it('should detect a binary operator with two operands is well formed', function() {
       assert.strictEqual(this._parser.isWellformed('a∧b'), true);
+    });
+
+    it('should be able to detect a unary operator with no operands is not well formed', function() {
+      assert.strictEqual(this._parser.isWellformed('¬'), false);
+    });
+
+    it('should be able to detect a unary operator with one operand is well formed', function() {
+      assert.strictEqual(this._parser.isWellformed('¬a'), true);
+    });
+
+    it('should be able to detect a unary operator with two operands is not well formed', function() {
+      assert.strictEqual(this._parser.isWellformed('a¬a'), false);
     });
   })
 });
